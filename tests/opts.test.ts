74013b1fe792d44f6b1da8d18904d366a2611770
bennesp <bennesp@users.noreply.github.com>
2025-04-22 14:43:45 +0200

    add automatic discovery of caches in Dockerfiles
    Signed-off-by: bennesp <bennesp@users.noreply.github.com>

--- tests/opts.test.ts
+++ tests/opts.test.ts
@@ -1,11 +1,13 @@
 import { expect, test } from 'vitest'
 import { getCacheMap, getTargetPath, getMountArgsString, parseOpts, getUID, getGID } from '../src/opts.js'
+import fs from 'fs'
 
 test('parseOpts with no arguments', () => {
     const opts = parseOpts([])
     expect(opts).toEqual({
         "_": [],
         "cache-map": "{}",
+        "dockerfile": "Dockerfile",
         "scratch-dir": "scratch",
         "skip-extraction": false,
         "extract": false,
@@ -21,6 +23,7 @@ test('parseOpts with cache-map argument', () => {
     expect(opts).toEqual({
         "_": [],
         "cache-map": '{"key": "value"}',
+        "dockerfile": "Dockerfile",
         "scratch-dir": "scratch",
         "skip-extraction": false,
         "extract": false,
@@ -36,6 +39,7 @@ test('parseOpts with deprecated cache-source and cache-target arguments', () => 
     expect(opts).toEqual({
         "_": [],
         "cache-map": '{"source":"target"}',
+        "dockerfile": "Dockerfile",
         "scratch-dir": "scratch",
         "skip-extraction": false,
         "extract": false,
@@ -53,6 +57,7 @@ test('parseOpts with utility-image argument', () => {
     expect(opts).toEqual({
         "_": [],
         "cache-map": '{}',
+        "dockerfile": "Dockerfile",
         "scratch-dir": "scratch",
         "skip-extraction": false,
         "extract": false,
@@ -68,6 +73,7 @@ test('parseOpts with builder argument', () => {
     expect(opts).toEqual({
         "_": [],
         "cache-map": '{}',
+        "dockerfile": "Dockerfile",
         "scratch-dir": "scratch",
         "skip-extraction": false,
         "extract": false,
@@ -78,11 +84,28 @@ test('parseOpts with builder argument', () => {
     })
 })
 
+test('parseOpts with dockerfile argument', () => {
+    const opts = parseOpts(['--dockerfile', 'Dockerfile.custom'])
+    expect(opts).toEqual({
+        "_": [],
+        "cache-map": "{}",
+        "dockerfile": "Dockerfile.custom",
+        "scratch-dir": "scratch",
+        "skip-extraction": false,
+        "extract": false,
+        "h": false,
+        "help": false,
+        "utility-image": "ghcr.io/containerd/busybox:latest",
+        "builder": "default"
+    })
+})
+
 test('parseOpts with help argument', () => {
     const opts = parseOpts(['-h'])
     expect(opts).toEqual({
         "_": [],
         "cache-map": "{}",
+        "dockerfile": "Dockerfile",
         "scratch-dir": "scratch",
         "skip-extraction": false,
         "extract": false,
@@ -97,6 +120,66 @@ test('getCacheMap', () => {
     const opts = parseOpts(['--cache-map', '{"key": "value"}'])
     const cacheMap = getCacheMap(opts)
     expect(cacheMap).toEqual({ key: 'value' })
+})
+
+test('getCacheMap with both cache-map and dockerfile specified', () => {
+    const opts = parseOpts(['--cache-map', '{"key": "value"}', '--dockerfile', 'Dockerfile.custom'])
+    const cacheMap = getCacheMap(opts)
+    expect(cacheMap).toEqual({ key: 'value' })
+})
+
+test('getCacheMapFromDockerfile', () => {
+    const tmpDir = fs.mkdtempSync('/tmp/dockerfile-test-');
+    const dockerfilePath = `${tmpDir}/Dockerfile`
+    fs.writeFileSync(dockerfilePath, `
+FROM alpine:latest AS builder
+
+# Target absolute path, no id
+RUN --mount=type=cache,target=/tmp/cache \
+    echo "Hello, World!" > /tmp/cache/hello.txt
+
+# Target relative path with workdir, no id
+WORKDIR /app
+RUN --mount=type=cache,target=cache1 \
+    echo "Hello, World!" > cache/hello.txt
+
+# Multi-stage build
+FROM alpine:latest
+
+# Target absolute path with id
+RUN --mount=type=cache,id=cache2,target=/tmp/cache \
+    echo "Hello, World!" > /tmp/cache/hello.txt
+
+# Target relative path with workdir and id
+WORKDIR /app2
+RUN --mount=type=cache,id=cache3,target=cache \
+    echo "Hello, World!" > cache/hello.txt
+`);
+
+    const opts = parseOpts(['--dockerfile', dockerfilePath])
+    const cacheMap = getCacheMap(opts)
+    fs.rmSync(tmpDir, { recursive: true })
+
+    expect(cacheMap).toEqual(
+        {
+            '/tmp/cache': {
+                'id': '/tmp/cache',
+                'target': '/tmp/cache'
+            },
+            'cache1': {
+                'id': 'cache1',
+                'target': '/app/cache1'
+            },
+            'cache2': {
+                'id': 'cache2',
+                'target': '/tmp/cache'
+            },
+            'cache3': {
+                'id': 'cache3',
+                'target': '/app2/cache'
+            }
+        }
+    )
 })
 
 test('getCacheMap with invalid JSON', () => {
